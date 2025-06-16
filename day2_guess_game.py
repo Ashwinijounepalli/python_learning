74418b1f7ad9c74210c313b319fb7460435a8b30
Ashwinijounepalli <ashwinijounepalli@gmail.com>
2025-06-16 17:19:04 +0530

    Update day2_guess_game.py

--- day2_guess_game.py
+++ day2_guess_game.py
@@ -1,12 +1,27 @@
 #Python picks a number (let’s say 7)
 #You guess the number
 #Python tells you if your guess is correct or not
-
+'''
 x=int(input("enter number: "))
 number=7
 if (x>7):
-    print(f"oh no, {x} Your guessing more than max digit!")
+    print(f"oh no, {x} Your guessing more than correct number!")
 elif (x<7):
-    print(f"oh no, {x} youre guessing less than min digitt!")
+    print(f"oh no, {x} youre guessing less than correct number!")
 else:
     print(f"hey {x} ,youre right!")
+'''
+
+#random number
+import random
+x=int(input("enter number: "))
+number=random.randint(1, 10)
+
+if (x>number):
+    print(f"oh no, {x} Your guessing more than correct number!")
+elif (x<number):
+    print(f"oh no, {x} youre guessing less than correct number!")
+else:
+    print(f"hey {x} ,youre right!")
+
+print(f"rand num is {number}")
